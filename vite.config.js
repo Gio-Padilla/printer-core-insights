bb2d27707fa08d6eb7af7a201c8a80f7f23d3702
Gio-Padilla <pad24005@byui.edu>
2025-11-24 19:03:51 -0600

    Update fixes

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/printer-core-insights/',
+  build: {
+    outDir: 'docs', // this tells Vite to build into docs/
+  }
 });
