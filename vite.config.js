5a87e80bd1577a97c5975bd471efedb17b7d6e16
Gio-Padilla <pad24005@byui.edu>
2025-12-09 01:24:15 -0600

    I think I got all the issues fixed so that it is working.

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import { defineConfig } from 'vite';
 export default defineConfig(({ mode }) => {
   return {
     root: 'src', // treat src/ as the root
-    base: mode === 'production' ? '/printer-core-insights/' : '/',
     build: {
-      outDir: '../docs', // output goes to docs/ for GitHub Pages
+      outDir: '../dist', // output goes to docs/ for GitHub Pages
       rollupOptions: {
         input: {
           main: resolve(__dirname, 'src/index.html'),
